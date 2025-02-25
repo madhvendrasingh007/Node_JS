915ddcfd23b1903585a727896282306ef86ddff5
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-02-25 22:15:36 +0000

    http server

---
+++ 01_basics/04_httpsServer.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} New Request Received`;
+    fs.appendFile('log.txt', log + '\n', (err) => {
+        switch(req.url) {
+            case '/': res.end('Homepage');
+            break;
+            case '/about': res.end('About Us');
+            break;
+            case '/contact': res.end('Contact Us');
+            break;
+            default: res.end('404 Page Not Found');
+        }
+    }
+    );
+    res.end('Hello from HTTPS Server');
+});
+
+myServer.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
