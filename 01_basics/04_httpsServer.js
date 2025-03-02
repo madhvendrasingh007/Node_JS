c719d53bd2d6171474221cf653d80f70f3203b0b
Madhvendra Singh <66571451+madhvendrasingh007@users.noreply.github.com>
2025-03-02 09:36:23 +0000

    initial commit

--- 01_basics/04_httpsServer.js
+++ 01_basics/04_httpsServer.js
@@ -1,21 +1,40 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Request Received`;
-    fs.appendFile('log.txt', log + '\n', (err, data) => {
-        switch(req.url) {
-            case '/': res.end('Homepage');
-            break;
-            case '/about': res.end('About Us');
-            break;
-            case '/contact': res.end('Contact Us');
-            break;
-            default: res.end('404 Page Not Found');
+    const log = `${Date.now()}: ${req.url} New Request Received\n`;
+
+    // Parse the URL from the incoming request
+    const myUrl = url.parse(req.url);
+
+    console.log(myUrl)
+
+    // Write the log entry to the file asynchronously
+    fs.appendFile('log.txt', log, (err) => {
+        if (err) {
+            console.error('Error writing to log file:', err);
         }
-    }
-    );
-    res.end('Hello from HTTPS Server');
+
+        // Switch based on the URL path
+        switch (req.url) {
+            case '/':
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('Homepage');
+                break;
+            case '/about':
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('About Us');
+                break;
+            case '/contact':
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('Contact Us');
+                break;
+            default:
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('404 Page Not Found');
+        }
+    });
 });
 
 myServer.listen(8000, () => {
